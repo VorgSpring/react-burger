bc9ce1e9a89d2be55fb01f76a62357a1fd97cd13
VorgSpring <drmrSP@yandex.ru>
2022-01-15 20:03:54 +0300

    prepared store

---
+++ src/services/reducers/initialState.js
@@ -0,0 +1,14 @@
+export const initialState = {
+  ingredients: {
+    items: null,
+    isLoading: false,
+    error: null,
+  },
+  burger: null,
+  viewedIngredient: null,
+  order: {
+    number: null,
+    isCreating: false,
+    error: null,
+  },
+};
---
+++ src/services/reducers/rootReducer.js
@@ -0,0 +1,3 @@
+import { initialState } from './initialState';
+
+export const rootReducer = (state = initialState) => state;
