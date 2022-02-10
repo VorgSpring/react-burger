c44ffd6591a9b2d7ff3c1695f55133a0a50ddcfc
VorgSpring <drmrSP@yandex.ru>
2022-02-10 23:44:35 +0300

    created route paths

---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+export const RoutePaths = {
+  MAIN: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  FORGOT_PASSWORD: '/forgot-password',
+  RESET_PASSWORD: '/reset-password',
+  PROFILE: '/profile',
+  INGREDIENT: '/ingredients/:id',
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
+import { RoutePaths } from '../constants/routes';
 
 export default () => (
   <Routes>
-    <Route path="/" element={<MainPage />} />
+    <Route path={RoutePaths.MAIN} element={<MainPage />} />
   </Routes>
 );
