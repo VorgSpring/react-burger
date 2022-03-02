d3dd56dce5d73f71d01e88a189c7ba99a4675e68
VorgSpring <drmrSP@yandex.ru>
2022-03-02 22:56:35 +0300

    created PasswordInput and EditableInput

---
+++ src/ui/EditableInput/EditableInput.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { Input } from '@ya.praktikum/react-developer-burger-ui-components';
+
+export const EditableInput = ({
+  name,
+  type,
+  value,
+  placeholder,
+  error,
+  errorText,
+  size,
+  onChange,
+}) => {
+  const [isEditable, setEditable] = useState(false);
+  const inputRef = useRef(null);
+
+  let typeInput = type;
+
+  if (type === 'password') {
+    typeInput = isEditable ? 'text' : 'password';
+  }
+
+  const handleIconClick = () => {
+    setEditable(!isEditable);
+    setTimeout(inputRef.current.focus, 0);
+  };
+
+  const handleBlur = () => {
+    setEditable(false);
+  };
+
+  return (
+    <Input
+      type={typeInput}
+      placeholder={placeholder}
+      onChange={onChange}
+      icon="EditIcon"
+      value={value}
+      name={name}
+      error={error}
+      ref={inputRef}
+      disabled={!isEditable}
+      onIconClick={handleIconClick}
+      onBlur={handleBlur}
+      errorText={errorText}
+      size={size}
+    />
+  );
+};
+
+EditableInput.defaultProps = {
+  size: 'default',
+};
+
+EditableInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  error: PropTypes.bool.isRequired,
+  errorText: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
---
+++ src/ui/PasswordInput/PasswordInput.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { Input } from '@ya.praktikum/react-developer-burger-ui-components';
+
+export const PasswordInput = ({
+  name,
+  value,
+  placeholder,
+  error,
+  errorText,
+  size,
+  onChange,
+}) => {
+  const [isVisible, setVisible] = useState(false);
+  const inputRef = useRef(null);
+
+  const type = isVisible ? 'text' : 'password';
+  const icon = isVisible ? 'HideIcon' : 'ShowIcon';
+
+  const handleIconClick = () => {
+    setVisible(!isVisible);
+    setTimeout(inputRef.current.focus(), 0);
+  };
+
+  const handleBlur = () => {
+    setVisible(false);
+  };
+
+  return (
+    <Input
+      type={type}
+      placeholder={placeholder}
+      onChange={onChange}
+      icon={icon}
+      value={value}
+      name={name}
+      error={error}
+      ref={inputRef}
+      onIconClick={handleIconClick}
+      onBlur={handleBlur}
+      errorText={errorText}
+      size={size}
+    />
+  );
+};
+
+PasswordInput.defaultProps = {
+  size: 'default',
+};
+
+PasswordInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  error: PropTypes.bool.isRequired,
+  errorText: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
